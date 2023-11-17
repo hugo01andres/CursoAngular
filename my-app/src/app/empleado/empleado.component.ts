b14fbe00af50eef718c28558bf66163c1e6f24fd
Hugo Martinez <72633652+hugo01andres@users.noreply.github.com>
2023-11-17 23:35:37 +0000

    Add empleado and empleados components

---
+++ my-app/src/app/empleado/empleado.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empleado',
+  templateUrl: './empleado.component.html',
+  styleUrl: './empleado.component.css'
+})
+export class EmpleadoComponent {
+  //Video 7
+
+}
