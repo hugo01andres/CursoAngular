c807b655311df1de7700cfa443714310fb084625
Hugo Martinez <72633652+hugo01andres@users.noreply.github.com>
2023-11-21 23:09:04 +0000

    Pruebo las directivas

--- trabajoDirectivaApp/src/app/app.component.ts
+++ trabajoDirectivaApp/src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+
+interface Entrada{
+  titulo: string
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,27 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'trabajoDirectivaApp';
+  title = 'Registro de usuarios';
+  mensaje = ''
+  registrado = false
+  sinRegistrar = true
+  nombre: string = ''
+  apellido: string = ''
+  entradas: Entrada[]
+
+  constructor(){
+
+    this.entradas = [
+      {titulo:"Python cada dia mas presente"},
+      {titulo:"Angular es el mejor framework"},
+      {titulo:"React es el mejor framework"},
+      {titulo:"Vue es el mejor framework"},
+    ]
+    console.log(this.entradas)
+  }
+
+  registrarUsuario(){
+    this.registrado = true
+    this.mensaje = 'Usuario registrado con exito'
+  }
 }
