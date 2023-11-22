34d64e2d4fc5d15f9fd5f05f1a8926cfe9426221
Hugo Martinez <72633652+hugo01andres@users.noreply.github.com>
2023-11-22 00:46:11 +0000

    Add employee list component and child component

--- app-empleados/src/app/app.module.ts
+++ app-empleados/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { EmpleadoHijoCComponent } from './empleado-hijo-c/empleado-hijo-c.component';
+import { CaracteristicasEmpleadoCComponent } from './caracteristicas-empleado-c/caracteristicas-empleado-c.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpleadoHijoCComponent,
+    CaracteristicasEmpleadoCComponent
   ],
   imports: [
     BrowserModule,
---
+++ app-empleados/src/app/caracteristicas-empleado-c/caracteristicas-empleado-c.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-caracteristicas-empleado-c',
+  templateUrl: './caracteristicas-empleado-c.component.html',
+  styleUrl: './caracteristicas-empleado-c.component.css'
+})
+export class CaracteristicasEmpleadoCComponent {
+
+}
